7bcae6626d7931d1800531ac400262bcb5eb803b
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-01-26 11:25:14 -0600

    Delete DialogBox directory

--- DialogBox/DialogBox.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class DialogBox {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Think of a number between one and ten.");
-        JOptionPane.showMessageDialog(null, "The number is " +
-        (1 + (int)(Math.random() * 10)) + ".");
-    }
-}
