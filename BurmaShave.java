90c2d817933a3bb83f044b01e6143d7df6f1827d
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-01-26 11:25:55 -0600

    Create BurmaShave.java

---
+++ BurmaShave.java
@@ -0,0 +1,9 @@
+public class BurmaShave {
+    public static void main(String[] args) {
+        System.out.println("Past schoolhouses");
+        System.out.println("Take it slow");
+        System.out.println("Let the little");
+        System.out.println("Shavers grow");
+        System.out.println("Burma Shave");
+    }
+}
