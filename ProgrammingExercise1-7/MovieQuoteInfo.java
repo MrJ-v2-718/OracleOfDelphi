b0c84ac9b4b7651db891bb634501c27f36ba3917
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-01-26 11:28:22 -0600

    Delete ProgrammingExercise1-7 directory

--- ProgrammingExercise1-7/MovieQuoteInfo.java
+++
@@ -1,8 +0,0 @@
-public class MovieQuoteInfo {
-    public static void main(String[] args) {
-        System.out.println("Accident is the first building block of evolution.");
-        System.out.println("from the movie Interstellar");
-        System.out.println("said by Dr. Amelia Brand");
-        System.out.println("in 2014");
-    }
-}
