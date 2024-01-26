4bc993d5640d5d9fc22484d590a8f1fba7b7e5a8
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-01-26 11:26:12 -0600

    Delete ProgrammingExercise1-11 directory

--- ProgrammingExercise1-11/BurmaShave.java
+++
@@ -1,9 +0,0 @@
-public class BurmaShave {
-    public static void main(String[] args) {
-        System.out.println("Past schoolhouses");
-        System.out.println("Take it slow");
-        System.out.println("Let the little");
-        System.out.println("Shavers grow");
-        System.out.println("Burma Shave");
-    }
-}
