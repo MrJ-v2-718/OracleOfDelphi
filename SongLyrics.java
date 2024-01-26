82b3d5544958829d6dc0fdc38b464df0578621e4
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-01-26 11:27:13 -0600

    Create SongLyrics.java

---
+++ SongLyrics.java
@@ -0,0 +1,8 @@
+public class SongLyrics {
+    public static void main(String[] args) {
+        System.out.println("Watashi wa watashi, anata wa anata to");
+        System.out.println("Yuube itteta sonna ki mo suru wa");
+        System.out.println("Gurei no jaketto ni");
+        System.out.println("Mioboe ga aru koohii no shimi");
+    }
+}
