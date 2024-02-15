549a877540f638a567dd8f174ca73ec4c8b04508
MrJ <144938009+MrJ-v2-718@users.noreply.github.com>
2024-02-15 15:06:46 -0600

    Add files via upload

---
+++ SpaServices/CreateSpaServices.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CreateSpaServices {
+	public static void main(String[] args) {
+		String service;
+		double price;
+		
+		SpaService firstService = new SpaService();
+		SpaService secondService = new SpaService();
+		SpaService thirdService = new SpaService("Facial", 49.99);
+		
+		firstService = getData(firstService);
+		// secondService = getData(secondService);
+		
+		System.out.println("First service details: ");
+		System.out.println(firstService.getServiceDescription() + 
+		" $" + firstService.getPrice());
+		
+		System.out.println("Second service details: ");
+		System.out.println(secondService.getServiceDescription() + 
+		" $" + secondService.getPrice());
+		
+		System.out.println("Third service details: ");
+		System.out.println(thirdService.getServiceDescription() + 
+		" $" + thirdService.getPrice());
+	}
+	
+	
+	public static SpaService getData(SpaService service) {
+		String serviceDescription;
+		double price;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Enter service: ");
+		serviceDescription = keyboard.nextLine();
+		System.out.print("Enter price: ");
+		price = keyboard.nextDouble();
+		keyboard.nextLine();
+		
+		service.setServiceDescription(serviceDescription);
+		service.setPrice(price);
+		return service;
+	}
+}
